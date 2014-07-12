949116ec6a6934c1338d634e7fe2a34b5de5b6da
jjw1707 <jjw1707@gmail.com>
2014-07-12 10:20:53 -0700

    asdfasdfasdf dosfjiodnfosfksnfko

--- game_server/game.js
+++ game_server/game.js
@@ -60,7 +60,7 @@ function no_move() {
   console.log("emit playerDied " + (turn_num % 4));
   gameSocket.emit('playerDied', {player_id: turn_num % 4});
   gameSocket.broadcast.emit('playerDied', {player_id: turn_num % 4});
-  playing[turn_num % 4] = false;
+  //playing[turn_num % 4] = false;
   if (game_valid) {
     get_next_valid_turn();
     console.log('emit playerTurn ' + (turn_num % 4));
@@ -71,9 +71,9 @@ function no_move() {
 
 function get_next_valid_turn() {
   turn_num = turn_num + 1;
-  while(playing[turn_num % 4] === false) {
-    turn_num = turn_num + 1;
-  }
+  //while(playing[turn_num % 4] === false) {
+  //  turn_num = turn_num + 1;
+  //}
 }
 
 function game_over() {
