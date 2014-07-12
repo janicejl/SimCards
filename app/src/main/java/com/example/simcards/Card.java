a73d338f1ac0e72d35a48860e0de76847619a257
Evy Kassirer <evy.kassirer@gmail.com>
2014-07-12 01:42:42 -0700

    small changes

--- app/src/main/java/com/example/simcards/Card.java
+++ app/src/main/java/com/example/simcards/Card.java
@@ -12,7 +12,7 @@ public class Card {
 	String suit;
 	int value;
 	int suits_picture_position;
-	
+
 	int x_pos;
 	int y_pos;
 
@@ -24,7 +24,7 @@ public class Card {
 	}
 	
 	String getSuit() {
-		return rank;
+		return suit;
 	}
 	int getX() {
 		return x_pos;
@@ -47,7 +47,7 @@ public class Card {
 		rank = rk;
 		suit = st;
 		suits_picture_position = suitVal;
-		
+
 		x_pos = card_width * (value - 1);
 		y_pos = card_height * suits_picture_position;
 	}
